62b99862e572a66f1c12b9fb56dff549bcca3099
Ignacio Arriagada <iarriagada@gemini.edu>
2020-10-12 14:16:11 -0400

    Added new zen option

--- yetanotherhelloworld.py
+++ yetanotherhelloworld.py
@@ -4,7 +4,9 @@ print('This is the Hello world greeter')
 print('How pumped do you want to feel?')
 while True:
     try:
-        enthus_level = input('Very(1)/Normal(2)/Meh(3)/Areyoukiddingis2020(4): ')
+        enthus_level = input('Zen(0)/Very(1)/Normal(2)/Meh(3)/Areyoukiddingis2020(4): ')
+        if int(enthus_level) is 0:
+            print('***Nothing can touch me***')
         if int(enthus_level) is 1:
             print('Hello World Yeah!!!')
         if int(enthus_level) is 2:
@@ -13,7 +15,7 @@ while True:
             print('\'sup world')
         if int(enthus_level) is 4:
             print('goodbye world x(')
-        elif int(enthus_level) not in [1,2,3,4]:
+        elif int(enthus_level) not in range(0,5):
             print('There\'s nothing I can do for you, goodbye')
         break
     except ValueError:
