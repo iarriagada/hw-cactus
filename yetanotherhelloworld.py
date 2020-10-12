f0cb30e721f2457611e2634e5d96905c7cc4003a
Ignacio Arriagada <iarriagada@gemini.edu>
2020-10-11 22:48:08 -0400

    new feature: user gets a random message

--- yetanotherhelloworld.py
+++ yetanotherhelloworld.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
+import random
+
+randmsg = ['Hello, is it me you are looking for',
+           'You say goodbye, I say hello. Hello, hello',
+           'Hello, can you hear me',
+           'Hello Darkness, my old friend',
+           'Goodbye, yellow brick road',
+           'Hey, I just met you, and this is crazy...',
+           'Hello, I love you, won\'t you tell me your name',
+           'Bye, bye, miss american pie']
 
 print('This is the Hello world greeter')
 print('How pumped do you want to feel?')
+
 while True:
     try:
-        enthus_level = input('Very(1)/Normal(2)/Meh(3)/Areyoukiddingis2020(4): ')
+        enthus_level = input('Very(1)/Normal(2)/Meh(3)/Areyoukiddingis2020(4)'\
+                             '/surpriseme(5): ')
         if int(enthus_level) is 1:
             print('Hello World Heck Yeah!!!')
         if int(enthus_level) is 2:
@@ -13,7 +25,10 @@ while True:
             print('\'sup world')
         if int(enthus_level) is 4:
             print('goodbye world x(')
-        elif int(enthus_level) not in [1,2,3,4]:
+        if int(enthus_level) is 5:
+            rand_pos = random.randint(0,7)
+            print(randmsg[rand_pos])
+        elif int(enthus_level) not in [1,2,3,4,5]:
             print('There\'s nothing I can do for you, goodbye')
         break
     except ValueError:
